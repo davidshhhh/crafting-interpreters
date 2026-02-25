cb6d836ed5ec5cc077c4a81ae4e8c63a32b34b31
DavidGitoo <143304527+DavidGitoo@users.noreply.github.com>
2026-02-24 18:22:36 -0800

    Implement break statement, division by zero check, and block comments

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -2,6 +2,8 @@
 
 package Book.com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static Book.com.craftinginterpreters.lox.TokenType.*;
@@ -12,21 +14,196 @@ class Parser {
     private final List<Token> tokens;
     private int current = 0;
 
+    // challenge question chpater 8
+    private boolean allowExpression;
+    private boolean foundExpression = false;
+
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
 
-    Expr parse() {
-        try {
-        return expression();
-        } catch (ParseError error) {
-        return null;
-        }
+    List<Stmt> parse() {
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
+      statements.add(declaration());
     }
 
+    return statements; 
+  }
+
+
     private Expr expression() {
-        return equality();
+        return assignment();
     }
+
+    private Stmt declaration() {
+    try {
+      if (match(VAR)) return varDeclaration();
+
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
+    private Stmt statement() {
+    if (match(IF)) return ifStatement();
+    if (match(FOR)) return forStatement();
+    if (match(WHILE)) return whileStatement();
+    if (match(PRINT)) return printStatement();
+    if (match(LEFT_BRACE)) return new Stmt.Block(block());
+
+    return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expect ';' after loop condition.");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(
+          Arrays.asList(
+              body,
+              new Stmt.Expression(increment)));
+    }
+
+    if (condition == null) condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    if (condition == null) condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    return body;
+  }
+
+  private Stmt ifStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'if'.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after if condition."); 
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Print(value);
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration.");
+    return new Stmt.Var(name, initializer);
+  }
+
+  private Stmt whileStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'while'.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after condition.");
+    Stmt body = statement();
+
+    return new Stmt.While(condition, body);
+  }
+
+  
+  private Stmt expressionStatement() {
+  Expr expr = expression();
+
+  if (allowExpression && isAtEnd()) {
+    foundExpression = true;
+  } else {
+    consume(SEMICOLON, "Expect ';' after expression.");
+  }
+  return new Stmt.Expression(expr);
+}
+
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after block.");
+    return statements;
+  }
+
+  private Expr assignment() {
+    //Expr expr = equality();
+    Expr expr = or();
+
+    if (match(EQUAL)) {
+      Token equals = previous();
+      Expr value = assignment();
+
+      if (expr instanceof Expr.Variable) {
+        Token name = ((Expr.Variable)expr).name;
+        return new Expr.Assign(name, value);
+      }
+
+      error(equals, "Invalid assignment target."); 
+    }
+
+    return expr;
+  }
+
+  private Expr or() {
+    Expr expr = and();
+
+    while (match(OR)) {
+      Token operator = previous();
+      Expr right = and();
+      expr = new Expr.Logical(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr and() {
+    Expr expr = equality();
+
+    while (match(AND)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Logical(expr, operator, right);
+    }
+
+    return expr;
+  }
+
 
     private Expr equality() {
         Expr expr = comparison();
@@ -96,6 +273,10 @@ class Parser {
       return new Expr.Literal(previous().literal);
     }
 
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
+    }
+
     if (match(LEFT_PAREN)) {
       Expr expr = expression();
       consume(RIGHT_PAREN, "Expect ')' after expression.");
@@ -160,6 +341,24 @@ class Parser {
     Lox.error(token, message);
     return new ParseError();
   }
+
+
+  Object parseRepl() {
+  allowExpression = true;
+  List<Stmt> statements = new ArrayList<>();
+  while (!isAtEnd()) {
+    statements.add(declaration());
+
+    if (foundExpression) {
+      Stmt last = statements.get(statements.size() - 1);
+      return ((Stmt.Expression) last).expression;
+    }
+
+    allowExpression = false;
+  }
+
+  return statements;
+}
 
   private void synchronize() {
     advance();
